0f4cf8b80b2286c0d72ba091b288561b4d3f3c27
Youssef Amar <youssefamar@icloud.com>
2020-06-27 19:32:46 +0200

    Projet complet

--- Banque/Form1.Designer.cs
+++ Banque/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Banque
 {
-    partial class Form1
+    partial class BanqueForm
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.rtbOuput = new System.Windows.Forms.RichTextBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(373, 524);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(119, 38);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "Entrer";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(12, 486);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(174, 25);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Entrer votre choix :";
+            // 
+            // rtbOuput
+            // 
+            this.rtbOuput.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rtbOuput.Font = new System.Drawing.Font("Quicksand", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbOuput.Location = new System.Drawing.Point(12, 12);
+            this.rtbOuput.Name = "rtbOuput";
+            this.rtbOuput.ReadOnly = true;
+            this.rtbOuput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.rtbOuput.Size = new System.Drawing.Size(480, 459);
+            this.rtbOuput.TabIndex = 2;
+            this.rtbOuput.Text = "";
+            this.rtbOuput.TextChanged += new System.EventHandler(this.rtbOuput_TextChanged);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput.Location = new System.Drawing.Point(17, 524);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(212, 30);
+            this.tbInput.TabIndex = 3;
+            // 
+            // BanqueForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(507, 590);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.rtbOuput);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnEnter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "BanqueForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Application banquaire";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.RichTextBox rtbOuput;
+        private System.Windows.Forms.TextBox tbInput;
     }
 }
 
--- Banque/Form1.cs
+++ Banque/Form1.cs
@@ -3,18 +3,185 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace Banque
 {
-    public partial class Form1 : Form
+    public partial class BanqueForm : Form
     {
-        public Form1()
+        private static int etat;
+        private Client c;
+        private XmlSerializer xs;
+        private StreamWriter wr;
+        private string fichier;
+
+        public BanqueForm()
         {
             InitializeComponent();
+            rtbOuput.SelectAll();
+            rtbOuput.SelectionAlignment = HorizontalAlignment.Center;
+            etat = 0;
+            Nom();
         }
+
+        public void Nom()
+        {
+            rtbOuput.AppendText("Veuillez entrer votre nom");
+            rtbOuput.AppendText("\n\n");
+        }
+
+        public void Start()
+        {
+            rtbOuput.AppendText("1. Voir montant\n2. Faire un virement\n3. Faire un retrait");
+            rtbOuput.AppendText("\n");
+            etat = 1;
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            switch (etat)
+            {
+                case 0: c = new Client(tbInput.Text); fichier = tbInput.Text + ".xml"; Start();  break;
+
+                case 1:
+
+                    int choix;
+                    bool verif = Int32.TryParse(tbInput.Text, out choix);
+
+                    if(!verif)
+                    {
+                        choix = 0;
+                    }
+
+                    switch (choix)
+                        {
+                        case 1: VoirMontant(); break;
+
+                        case 2: etat = 2; MessageVirement(); break;
+
+                        case 3: etat = 3; MessageRetrait(); break;
+
+                        default : rtbOuput.AppendText("\nVeuillez entrer un choix correct\n"); break;
+
+                    } break;
+
+                case 2: try { Virement(Double.Parse(tbInput.Text)); } catch { rtbOuput.AppendText("\nVeuillez entrer une valeur\n"); etat = 1; } ; break;
+
+                case 3: try { Retrait(Double.Parse(tbInput.Text)); } catch { rtbOuput.AppendText("\nVeuillez entrer une valeur\n"); etat = 1; }; break;
+
+            }
+
+            tbInput.Clear();
+        }
+
+        public void VoirMontant()
+        {
+            rtbOuput.AppendText("\n\n");
+            rtbOuput.AppendText("Vous avez "+ c.Montant.ToString() + " euros sur votre compte\n");
+            rtbOuput.AppendText("\n");
+            Start();
+        }
+
+        public void MessageVirement()
+        {
+            rtbOuput.AppendText("\n");
+            rtbOuput.AppendText("Veuillez entrer le montant du virement en euros");
+            rtbOuput.AppendText("\n");
+        }
+
+        public void MessageRetrait()
+        {
+            rtbOuput.AppendText("\n");
+            rtbOuput.AppendText("Veuillez entrer le montant du retrait en euros");
+            rtbOuput.AppendText("\n");
+        }
+
+        public void Virement(double v)
+        {
+            c.FaireVirement(v);
+            rtbOuput.AppendText("\n\nVirement effectué !");
+            VoirMontant();
+        }
+
+        public void Retrait(double r)
+        {
+            if(r < c.Montant )
+            {
+                c.FaireRetrait(r);
+                rtbOuput.AppendText("\n\nRetrait effectué !");
+            }
+            else
+                rtbOuput.AppendText("\n\nFond insuffisant !");
+
+            VoirMontant();
+        }
+
+        private void rtbOuput_TextChanged(object sender, EventArgs e)
+        {
+            rtbOuput.ScrollToCaret();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            try
+            {
+                xs = new XmlSerializer(c.GetType());
+
+                wr = new StreamWriter(fichier);
+
+                xs.Serialize(wr, c);
+            }
+            catch { base.OnFormClosing(e); }
+
+            base.OnFormClosing(e);
+        }
+
     }
+
+    public class Client
+    {
+        private string nom;
+        private double montant;
+        private List<double> virement, retrait;
+
+        public Client()
+        {
+            this.nom = "";
+            this.montant = 0;
+            virement = new List<double>();
+            retrait = new List<double>();
+        }
+
+        public Client(string n)
+        {
+            this.nom = n;
+            this.montant = 0;
+            virement = new List<double>();
+            retrait = new List<double>();
+        }
+
+        public void FaireVirement(double v)
+        {
+            virement.Add(v);
+            montant = montant + v;
+        }
+
+        public void FaireRetrait(double r)
+        {
+            retrait.Add(r);
+            montant = montant - r;
+        }
+
+        public double Montant { get => montant; set => montant = value; }
+        public List<double> Virement { get => virement; set => virement = value; }
+        public List<double> Retrait { get => retrait; set => retrait = value; }
+        public string Nom { get => nom; set => nom = value; }
+    }
+
 }
